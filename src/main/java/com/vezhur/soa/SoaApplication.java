72bd2ad99c7797080860c3a0e5ff733290b0a8b8
vecheslav <vezhur2003@gmail.com>
2024-12-06 13:08:18 +0300

    ой забыл еще один коммит (обнова ssl)

--- src/main/java/com/vezhur/soa/SoaApplication.java
+++ src/main/java/com/vezhur/soa/SoaApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoaApplication {
     public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.trustStore", "./certs/spring.truststore");
+        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+        System.setProperty("jsse.enableSNIExtension", "false");
         SpringApplication.run(SoaApplication.class, args);
     }
 }
