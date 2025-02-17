e10f3530fb92dcf1ad29078180e04b2de8593e80
vyachessslav <vezhur2003@gmail.com>
2025-02-17 23:21:48 +0300

    lab3

--- src/main/java/com/vezhur/soa/SoaApplication.java
+++ src/main/java/com/vezhur/soa/SoaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoaApplication {
     public static void main(String[] args) {
-        System.setProperty("javax.net.ssl.trustStore", "./certs/spring.truststore");
-        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+        //System.setProperty("javax.net.ssl.trustStore", "./certs/spring.truststore");
+        //System.setProperty("javax.net.ssl.trustStorePassword", "password");
         System.setProperty("jsse.enableSNIExtension", "false");
         SpringApplication.run(SoaApplication.class, args);
     }
